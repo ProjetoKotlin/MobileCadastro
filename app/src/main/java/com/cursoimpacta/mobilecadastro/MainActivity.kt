a3c0db70830bb3013013f2c630871b7579ec45e8
Rodrigo Mazucato <rodrigolopes.sp2005@gmail.com>
2024-05-16 17:57:53 -0300

    Criação de funções para validar os campos

--- app/src/main/java/com/cursoimpacta/mobilecadastro/MainActivity.kt
+++ app/src/main/java/com/cursoimpacta/mobilecadastro/MainActivity.kt
@@ -4,17 +4,23 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,12 +29,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import com.cursoimpacta.mobilecadastro.ui.theme.MobileCadastroTheme
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.util.regex.Pattern
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,61 +57,126 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun validaNome(nome: String): String {
+    val lista = nome.split(" ")
+    if (lista.size >= 2) {
+        for (palavra in lista) {
+            if (palavra.length < 3) {
+                return "Cada nome deve conter no mínimo 3 letras cada!"
+            }
+        }
+        return "Certo"
+    } else {
+        return "O nome precisa de no mínimo 2 palavras!"
+    }
+}
+
+fun validaEmail(email: String): String{
+    val regex = Regex("[A-Z|a-z|0-9|\\.|_|-]+@[\\w-]+\\.[a-z]{2,}")
+    if (regex.matches(email))
+        return "Email válido"
+    else{
+        return "Email inválido"
+    }
+}
+
+fun validaTelefone(telefone: String): String{
+    if (telefone.length == 11){
+        return "Telefone válido"
+    }
+    else {
+        return "O telefone deve conter 11 dígitos"
+    }
+}
+
 @Composable
 fun App() {
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        CampoUsuario()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFD1E0F2)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CampoUsuario(false)
     }
 
 }
 
+
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun CampoUsuario() {
+fun CampoUsuario(registrado: Boolean) {
     val api = remember { ViaCepService.create() }
     val coroutineScope = rememberCoroutineScope()
-    var dados by remember {
-        mutableStateOf<DadosPessoais?>(null)
+    val dados by remember {
+        mutableStateOf(DadosPessoais("", "", "", "9", "", ""))
     }
-    var cep by remember {
-        mutableStateOf("")
-    }
+
     var infoCep by remember {
         mutableStateOf<Endereco?>(null)
     }
+    val colorWhite = TextFieldDefaults.colors(
+        focusedContainerColor = Color.White,
+        unfocusedContainerColor = Color.White,
+        disabledContainerColor = Color.White,
+    )
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(25.dp)
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+        modifier = Modifier
+            .width(280.dp)
+            .clip(RoundedCornerShape(8.dp))
     ) {
-        TextField(
-            value = dados?.nome ?: "",
-            onValueChange = { dados?.nome = it },
-            label = { Text(text = "Nome") })
 
         TextField(
-            value = dados?.email ?: "",
-            onValueChange = { dados?.email = it },
-            label = { Text(text = "Email") })
+            dados.nome, { dados.nome = it }, Modifier.clip(RoundedCornerShape(8.dp)),
+            label = { Text(text = "Nome") },
 
-        TextField(value = cep, onValueChange = {
+            colors = colorWhite
+        )
+
+        Text(text = validaNome(dados.nome))
+
+        TextField(
+            value = dados.email,
+            onValueChange = { dados.email = it },
+            label = { Text(text = "Email") },
+
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            colors = colorWhite
+        )
+        Text(text = validaEmail(dados.email))
+
+        Text(
+            text = "Endereço:",
+            modifier = Modifier
+                .align(Alignment.Start)
+        )
+        TextField(value = dados.cep, onValueChange = {
             if (it.isDigitsOnly()) {
-                cep = it
+                dados.cep = it
             }
-        }, label = { Text(text = "CEP") })
-
-        if (cep.length == 8) {
+        }, label = { Text(text = "CEP") },
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp)),
+            colors = colorWhite,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        var msgStatus = ""
+        if (dados.cep.length == 8) {
             coroutineScope.launch {
+                msgStatus = "Loading"
                 lateinit var response: Response<Endereco>
-                response = api.procuraCep(cep)
-                infoCep = if (response.isSuccessful && !response.body()!!.erro) {
-                    response.body()
+                response = api.procuraCep(dados.cep)
+                if (response.isSuccessful && !response.body()!!.erro) {
+                    infoCep = response.body()
                 } else {
-                    null
+                    msgStatus = "CEP inválido!"
                 }
             }
             if (infoCep == null) {
-                Text(text = "CEP inválido")
+                Text(text = msgStatus)
             }
         }
 
@@ -112,35 +187,57 @@ fun CampoUsuario() {
                 Text(
                     text = "Logradouro"
                 )
-            })
-        TextField(value = infoCep?.bairro ?: "", onValueChange = { infoCep?.bairro = it }, label = {
-            Text(
-                text = "Bairro"
-            )
-        })
-        Row {
+            },
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            colors = colorWhite
+        )
+
+        TextField(
+            value = infoCep?.bairro ?: "",
+            onValueChange = { infoCep?.bairro = it },
+            label = {
+                Text(
+                    text = "Bairro"
+                )
+            },
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            colors = colorWhite
+        )
+        Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
             TextField(
-                value = dados?.numero ?: "",
-                onValueChange = { dados?.numero = it },
+                value = dados.numero,
+                onValueChange = {
+                    if (it.isDigitsOnly()) {
+                        dados.numero = it
+                    }
+                },
                 label = {
                     Text(
                         text = "Número"
                     )
                 },
-                modifier = Modifier.width(150.dp)
+                modifier = Modifier
+                    .width(110.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             TextField(
-                value = dados?.complemento ?: "",
-                onValueChange = { dados?.complemento = it },
+                value = dados.complemento,
+                onValueChange = {dados.complemento = it},
                 label = {
                     Text(
                         text = "Complemento"
                     )
                 },
-                modifier = Modifier.width(150.dp)
-            )
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite,
+
+                )
         }
-        Row {
+        Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
             TextField(
                 value = infoCep?.localidade ?: "",
                 onValueChange = { infoCep?.localidade = it },
@@ -148,7 +245,12 @@ fun CampoUsuario() {
                     Text(
                         text = "Cidade"
                     )
-                })
+                },
+                modifier = Modifier
+                    .width(150.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite
+            )
             TextField(
                 value = infoCep?.uf ?: "",
                 onValueChange = { infoCep?.uf = it },
@@ -156,22 +258,58 @@ fun CampoUsuario() {
                     Text(
                         text = "Estado"
                     )
-                })
-
-            Row {
-                TextField(
-                    value = infoCep?.ddd ?: "",
-                    onValueChange = { infoCep?.ddd = it },
-                    label = { Text(text = "Nome") })
-                TextField(
-                    value = dados?.telefone ?: "",
-                    onValueChange = { dados?.telefone = it },
-                    label = { Text(text = "Telefone") })
-            }
+                },
+                modifier = Modifier
+                    .width(110.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite
+            )
         }
+        Text(text = "Contato:", modifier = Modifier.align(Alignment.Start))
+        Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+            TextField(
+                value = infoCep?.ddd ?: "",
+                onValueChange = {
+                    if (it.isDigitsOnly()) {
+                        infoCep?.ddd = it
+                    }
+                },
+                label = { Text(text = "DDD") },
+                modifier = Modifier
+                    .width(70.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            TextField(
+                value = dados.telefone,
+                onValueChange = {
+                    if (it.isDigitsOnly()) {
+                        dados.telefone = it
+                    }
+                },
+                label = { Text(text = "Telefone") },
+                modifier = Modifier
+                    .width(190.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                colors = colorWhite,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
 
-
+            Text(text = validaTelefone(dados.telefone))
+        }
     }
+    Spacer(modifier = Modifier.height(40.dp))
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(280.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF809CBE))
+    ) {
+        Text(text = "CADASTRAR")
+    }
+
 }
 
 data class Endereco(
@@ -186,6 +324,7 @@ data class Endereco(
 data class DadosPessoais(
     var nome: String,
     var email: String,
+    var cep: String,
     var telefone: String,
     var numero: String,
     var complemento: String
