4c62cb1f387f5fc4cdb99c5318c91e8a692ad636
Rodrigo Mazucato <rodrigolopes.sp2005@gmail.com>
2024-05-26 02:50:43 -0300

    Finalização do projeto 🎉🎉🎉🎉

--- app/src/main/java/com/cursoimpacta/mobilecadastro/Camera.kt
+++ app/src/main/java/com/cursoimpacta/mobilecadastro/Camera.kt
@@ -7,12 +7,10 @@ import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -25,7 +23,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import java.io.IOException
